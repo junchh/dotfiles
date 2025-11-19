ff5313b9cf9e0996e851b90ad61418012051676a
junchh <cjunho9868@gmail.com>
2025-11-19 23:05:05 +0700

    feat: add same directory when creating new window

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,8 @@ set -g fish_greeting
 fish_config theme choose "Rosé Pine Moon"
 set -gx PATH /Users/junchh/.cargo/bin $PATH
 starship init fish | source
+
+# ZVM
+set -gx ZVM_INSTALL "$HOME/.zvm/self"
+set -gx PATH $PATH "$HOME/.zvm/bin"
+set -gx PATH $PATH "$ZVM_INSTALL/"
